877ce655a3ae3b370c28b11b22f9349c454d3ef4
Ruslan Rezyapov <rruslan97@yandex.ru>
2023-01-24 16:53:38 +0500

    123

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnNode = document.querySelector('.js-btn');
+const trafficLightNode = document.querySelector('.js-traffic-light');
+
+
+
+initLight(trafficLightNode);
+
+btnNode.addEventListener('click', function () {
+    switchLight(trafficLight);
+});
+
